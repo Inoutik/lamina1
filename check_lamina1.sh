ed62ae1da3f7425fcfee56b1c8ac6432562ff4c0
Inoutik <64523513+Inoutik@users.noreply.github.com>
2023-06-21 10:50:23 +0300

    Update check_lamina1.sh
    Add side chains + cosmetics

--- check_lamina1.sh
+++ check_lamina1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+localhost=127.0.0.1:9650
+lamina1rpc=rpc-testnet.lamina1.com
 echo '⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⣿⣿⣿'
 echo '⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣤⠀⣿⣿⣿'
 echo '⣿⠿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⢿⣿⣿⣿⣿⣿⡿⠿⣿⣿⡿⠿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⣿⣿⠿⢿⣿⣿⠿⣿⣿⣿⣿⣿⣿⡿⠿⣿⣿⣿⣿⠛⠀⢻⣿⣿'
@@ -7,40 +9,48 @@ echo '⣿⠀⠿⠿⢿⣿⣿⣿⣿⠇⢀⣉⡀⠸⣿⣿⣿⣿⡇⠀⣦⣤⡆⠀�
 echo '⣿⣶⣶⣶⣾⣿⣿⣿⣿⣶⣾⣿⣷⣶⣿⣿⣿⣿⣷⣶⣿⣿⣷⣶⣿⣿⣿⣿⣿⣶⣶⣶⣿⣿⣿⣿⣿⣶⣿⣿⣷⣶⣿⣿⣿⣿⣷⣶⣿⣿⣷⣾⣿⣿⣿⣿⣿⣿⣿'
 echo ''
 echo 'You node ID'
-node_id=$(curl -s -X POST --data '{"jsonrpc":"2.0", "id" :1,"method" :"info.getNodeID"}' -H 'content-type:application/json;' 127.0.0.1:9650/ext/info | cut -d '"' -f 10)
+node_id=$(curl -s -X POST --data '{"jsonrpc":"2.0", "id" :1,"method" :"info.getNodeID"}' -H 'content-type:application/json;' $localhost/ext/info | cut -d '"' -f 10)
 echo $node_id
 echo 'healty - ' | tr -d '\n'
-curl -s -X POST --data '{"jsonrpc":"2.0","id":1,"method" :"health.health"}' -H 'content-type:application/json;' 127.0.0.1:9650/ext/health | jq '.result.healthy'
+curl -s -X POST --data '{"jsonrpc":"2.0","id":1,"method" :"health.health"}' -H 'content-type:application/json;' $localhost/ext/health | jq '.result.healthy'
 echo ''
 echo 'Current Lamina1 block on node'
-curl -s -X POST --data '{ "jsonrpc":"2.0", "id":1, "method" :"eth_blockNumber", "params":[]}' -H 'content-type:application/json;' localhost:9650/ext/bc/C/rpc | jq '.result'
+localblock=$(curl -s -X POST --data '{ "jsonrpc":"2.0", "id":1, "method" :"eth_blockNumber", "params":[]}' -H 'content-type:application/json;' $localhost/ext/bc/C/rpc | jq '.result')
+echo -n $localblock; echo -n " - "; echo $localblock | xargs printf "%d"
+echo ''
 echo ''
 echo 'Current Lamina1 block in blockchain'
-curl -s -X POST --data '{ "jsonrpc":"2.0", "id":1, "method" :"eth_blockNumber", "params":[]}' -H 'content-type:application/json;' https://rpc-testnet.lamina1.com/ext/bc/C/rpc | jq '.result'
+lamina1block=$(curl -s -X POST --data '{ "jsonrpc":"2.0", "id":1, "method" :"eth_blockNumber", "params":[]}' -H 'content-type:application/json;' https://$lamina1rpc/ext/bc/C/rpc | jq '.result')
+echo -n $lamina1block; echo -n " - "; echo $lamina1block | xargs printf "%d"
+echo ''
 echo ''
 echo 'Your node uptime (node info)'
-curl -s -X POST --data '{"jsonrpc":"2.0","id":1,"method" :"info.uptime"}' -H 'content-type:application/json;' 127.0.0.1:9650/ext/info | jq '.result'
+curl -s -X POST --data '{"jsonrpc":"2.0","id":1,"method" :"info.uptime"}' -H 'content-type:application/json;' $localhost/ext/info | jq '.result'
 echo ''
 echo 'Your node uptime (Lamina1 rpc info)'
-uptime=$(curl -sX POST --data '{"jsonrpc": "2.0","method": "platform.getCurrentValidators","params": {},"id": 1}' -H 'content-type:application/json;' https://rpc-testnet.lamina1.com/ext/bc/P|jq -rC '.result.validators[]|select(.nodeID=="'$node_id'")|.uptime')
-echo "$uptime*100" | bc -l
+uptime=$(curl -sX POST --data '{"jsonrpc": "2.0","method": "platform.getCurrentValidators","params": {},"id": 1}' -H 'content-type:application/json;' https://$lamina1rpc/ext/bc/P|jq -rC '.result.validators[]|select(.nodeID=="'$node_id'")|.uptime')
+echo "$uptime" | bc -l
 echo ''
 echo 'Number of connected peers'
-curl -s -X POST --data '{"jsonrpc":"2.0","id":1,"method" :"info.peers"}' -H 'content-type:application/json;' 127.0.0.1:9650/ext/info|jq '.result.numPeers'
+curl -s -X POST --data '{"jsonrpc":"2.0","id":1,"method" :"info.peers"}' -H 'content-type:application/json;' $localhost/ext/info|jq '.result.numPeers'
 echo ''
+cd /etc/lamina1/configs/testnet5/
+a=$(curl -sS -X POST --data '{"jsonrpc": "2.0","method": "platform.getBlockchains","params": {},"id": 1}' -H 'content-type:application/json;' http://$localhost/ext/bc/P |jq .result.blockchains[])
+subnets=($(jq -r '."track-subnets"' < default.json))
+[[ -e local.json ]] && subnets+=($(jq -r '."track-subnets"' < local.json))
+bcids=()
+for subnet in ${subnets[*]};do 
+   bcids+=($(jq -r <<<"$a"    'select(.subnetID=="'$subnet'").id' ))
+done
 echo 'Is your node bootstrapped?'
-starting_port=$1
-[[ $starting_port ]] || starting_port=9650
-for n in {0..0};do 
-    for c in C P X;do
-        echo $c 'chain - ' | tr -d '\n'
-        curl -s -X POST --data '{
-            "jsonrpc": "2.0",
-            "method": "info.isBootstrapped",
-            "params":{
-             "chain":"'$c'"
-            },
-            "id": 1
-        }' -H 'content-type:application/json;' 127.0.0.1:$((starting_port+n*10))/ext/info | jq '.result.isBootstrapped' 
-    done
+for c in C P X ${bcids[*]};do
+echo $c 'chain - ' | tr -d '\n'
+curl -sS -X POST --data '{
+    "jsonrpc": "2.0",
+    "method": "info.isBootstrapped",
+    "params":{
+     "chain":"'$c'"
+    },
+    "id": 1
+}' -H 'content-type:application/json;' $localhost/ext/info | jq '.result.isBootstrapped' 
 done
